8ec19c72bfa65cd94b3d2b40f9076f9101e1e5e6
Marienne-ma <marianna.masala91@gmail.com>
2026-06-18 12:56:15 +0200

    update cardComponent

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from '../app/features/component/navbar-component/navbar-component';
 import { FooterComponent } from '../app/features/component/footer-component/footer-component';
+import { RegionTagPageComponent } from './features/page/region-tag-page-component/region-tag-page-component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent, FooterComponent],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent, RegionTagPageComponent],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
