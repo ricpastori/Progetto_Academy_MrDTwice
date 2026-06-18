48046f11f932b2aa8ef677cb0e044706f7728179
Riccardo Pastori <ricpastori@icloud.com>
2026-06-18 16:55:24 +0200

    Collega le route API backend

--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,14 @@ const express = require('express');
 const cors = require('cors');
 const db = require('./db');
 const apiService = require('./src/services/ApiService');
+const apiRoutes = require('./src/routes/ApiRoutes');
 
 const app = express();
 const port = 8080;
 
 app.use(cors());
 app.use(express.json());
+app.use(apiRoutes);
 
 async function startServer() {
   try {
@@ -21,7 +23,6 @@ async function startServer() {
     app.listen(port, () => {
       console.log(`Server avviato su http://localhost:${port}`);
     });
-
   } catch (error) {
     console.error('Errore connessione Supabase:', error.message);
     process.exit(1);
--- backend/src/routes/ApiRoutes.js
+++ backend/src/routes/ApiRoutes.js
@@ -7,7 +7,7 @@ const apiService = require('../services/ApiService');
 //GET REGIONS
 router.get(`/api/regions`, async (req, res) => {
   try {
-    const regions = apiService.getRegions();
+    const regions = await apiService.getRegions();
 
     res.status(200).json(regions);
   } catch (error) {
@@ -18,7 +18,7 @@ router.get(`/api/regions`, async (req, res) => {
 //GET TAGS
 router.get(`/api/tags`, async (req, res) => {
   try {
-    const tags = apiService.getTags();
+    const tags = await apiService.getTags();
 
     res.status(200).json(tags);
   } catch (error) {
@@ -70,7 +70,7 @@ router.get('/api/content/by-tag/top-liked-by-region', async (req, res) => {
 //GET SUB_TAGS
 router.get(`/api/sub-tags`, async (req, res) => {
   try {
-    const subTags = apiService.getSubTags();
+    const subTags = await apiService.getSubTags();
 
     res.status(200).json(subTags);
   } catch (error) {
