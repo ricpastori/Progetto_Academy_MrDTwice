d2b7edebc3c303881a1ff3ac9dcf71e7592c2cc2
Marienne-ma <marianna.masala91@gmail.com>
2026-06-18 16:37:44 +0200

    update pagina Regione Tag

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -2,7 +2,7 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from '../app/features/component/navbar-component/navbar-component';
 import { FooterComponent } from '../app/features/component/footer-component/footer-component';
-import { RegionTagPageComponent } from './features/page/region-tag-page-component/region-tag-page-component';
+import { RegionTagPageComponent } from './features/page/region-tag-page/region-tag-page';
 
 @Component({
   selector: 'app-root',
