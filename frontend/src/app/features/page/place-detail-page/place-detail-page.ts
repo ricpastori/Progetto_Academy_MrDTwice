8081de043c53e9a6c74ff2bad43df7d7c095bd40
Riccardo Pastori <ricpastori@icloud.com>
2026-06-19 16:16:02 +0200

    Remove orphan page

--- frontend/src/app/features/page/place-detail-page/place-detail-page.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-place-detail-page',
-  imports: [],
-  templateUrl: './place-detail-page.html',
-  styleUrl: './place-detail-page.css',
-})
-export class PlaceDetailPage {}
